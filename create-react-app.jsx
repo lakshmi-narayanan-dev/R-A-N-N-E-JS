04c8bf68bcbc804d0b449554c7266eaaf0e6fcac
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-20 10:36:47 +0530

    Update create-react-app.jsx

--- create-react-app.jsx
+++ create-react-app.jsx
@@ -1,1 +1,18 @@
+---------------------------------------------------------------------
+ReactJS Project Install + Load
+---------------------------------------------------------------------
+1. sudo npm --location=global install create-react-app for installing globally & check create-react-app --version
 
+2. npx create-react-app folder name to create a folder of predefined npm
+scripts to execute react project
+
+3. Once installation is completed , happy hacking ! message will be displayed
+and open the terminal cd folder name && code . && npm start.
+
+Alternative method from CRA:
+4. https://medium.com/@dawid.niegrebecki/create-react-app-is-dead-what-to-use-instead-fcdd46b70295#:~:text=It%20was%20the%20easiest%20way,solution%20for%20creating%20React%20applications. 
+
+https://liaogg.medium.com/its-time-to-stop-using-create-react-app-a99917dbfc  
+
+npm create vite@latest
+React install is a better way.
