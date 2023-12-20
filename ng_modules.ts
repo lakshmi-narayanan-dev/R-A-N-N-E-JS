174666bfe96ced82c157c542d53b1065f3133a9a
LAKSHMI NARAYANAN R <142524733+lakshmi-narayanan-dev@users.noreply.github.com>
2023-12-20 10:38:17 +0530

    Create ng_modules.ts

---
+++ ng_modules.ts
@@ -0,0 +1,13 @@
+---------------------------------------------------------------------
+Angular Installation
+---------------------------------------------------------------------
+1. https://tecadmin.net/how-to-install-angular-cli-on-ubuntu-22-04/  
++
+2. https://stackoverflow.com/questions/49679808/error-eacces-permission-denied 
+
+1. sudo npm install -g @angular/cli
+2. If there is any issue in installation of file access not allowed
+3. sudo npm install -g @angular/cli --unsafe-perm=true --allow-root
+4. ng version check angular version
+5. mkdir angular folder
+6. ng new project-folder / to install all dependencies to run the application
